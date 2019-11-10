8e2b5f92623266eef813caff85f33be3ba586147
Rafi92Stern <55682514+Rafi92Stern@users.noreply.github.com>
2019-11-10 15:50:17 +0200

    Remove duplicated directory

--- realtore_statat/realtore/src/new_componenet/gallary/gallary_img.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-class GallaryImg extends React.Component{
-    render(){
-        return(<div className="Gallary-img"><img src={this.props.src}/></div>);
-    }
-}
-export default GallaryImg;
--- realtore_statat/realtore/src/new_componenet/gallary/gallarydetails.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-class GallarymenDetails extends React.Component{
-    render(){
-        return(<div className="Gallary-header"><p>{this.props.header}</p></div>);
-    }
-}
-export default GallarymenDetails;
